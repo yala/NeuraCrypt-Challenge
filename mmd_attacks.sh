60342fe8ec85caf759b1e65bcaf7d3553c55fcd3
Adam Yala <adamyala1@gmail.com>
2021-06-28 01:09:08 -0400

    Update mmd_attacks.sh

--- mmd_attacks.sh
+++ mmd_attacks.sh
@@ -1,3 +1,4 @@
+cd NeuraCrypt
 ## Run MMD Attack on Linear encoding
 python scripts/main.py --batch_size 32 --dataset stanford_cxr_edema  --gpus 1 --private --use_adv  --use_mmd_adv  --encoded_data_dir /Mounts/rbg-storage1/users/adamyala/neuracrypt_embeddings/linear  --private_depth -1 --load_data_from_encoded_dir  --remove_pixel_shuffle --test_on_encoded_dir --train --test --weight_decay 0.001 --init_lr 1e-04 --max_epochs 25 --results_path logs/linear_mmd_attack.results
 
