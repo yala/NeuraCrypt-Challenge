6f04a19475d0604216521a583007314b9d0279b5
Adam Yala <adamyala1@gmail.com>
2021-06-28 01:10:55 -0400

    Update create_challenge_submission.sh

--- create_challenge_submission.sh
+++ create_challenge_submission.sh
@@ -1,3 +1,4 @@
+cd NeuraCrypt
 ## Creating demo submission
 python scripts/create_submission.py --batch_size 256 --gpus 1  --encoded_data_dir /Mounts/rbg-storage1/users/adamyala/neuracrypt_embeddings/neuracrypt-7 --load_data_from_encoded_dir --submission_dir /Mounts/rbg-storage1/users/adamyala/neuracrypt_challenge/sample_submission
 
